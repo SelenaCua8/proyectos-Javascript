bca28f6de83fc595b0c5c4daa4d73eaf822a0ec3
Selena Cuadra <selenac99@hotmail.com>
2023-03-18 00:02:07 -0300

    agregando comentarios

--- proyectosJavaScript/proyecto4/contador.js
+++ proyectosJavaScript/proyecto4/contador.js
@@ -6,5 +6,5 @@ let contador = 0;
 btn.addEventListener("click",() =>{  // desde boton, llamamos al evento click, declaramos una variable como contador y cada vez
     /* console.log("click"); */       // que se le hace click se le suma ++ un numero 
     contador++;
-   parrafo.textContent = contador;
+   parrafo.textContent = contador; //textContent es que vos estas cambiando el parrafo por lo que pasa en contador
 })
