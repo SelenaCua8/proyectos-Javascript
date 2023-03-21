82d9dc663530aab7f13ab9b7fd05e2c8323f33e5
Selena Cuadra <selenac99@hotmail.com>
2023-03-20 23:06:50 -0300

    ultimando detalles css

--- proyectosJavaScript/proyecto5/todolist.js
+++ proyectosJavaScript/proyecto5/todolist.js
@@ -1,13 +1,48 @@
+
+
+// Utilizando SweetAlert 
+(async () => {
+
+    const { value: accept } = await Swal.fire({
+      title: '¿Usted acepta los terminos y condiciones?',
+      input: 'checkbox',
+      inputValue: 1,
+      inputPlaceholder:
+        'Estoy de acuerdo con los terminos y condiciones',
+      confirmButtonText:
+        'Continue <i class="fa fa-arrow-right"></i>',
+      inputValidator: (result) => {
+        return !result && 'Necesitas aceptar para poder seguir'
+      }
+    })
+    
+    if (accept) {
+      Swal.fire('Bienvenido a: "Organizador de tareas"')
+    }
+    
+    })();
+
+
+
+
+
+//EMPIEZA TO DO LIST
 const input = document.querySelector('input');
 const addBtn = document.querySelector('.btn-add');
 const ul = document.querySelector('ul');
 const empty = document.querySelector('.empty');
+const span = document.querySelector('span');
+
+
+ 
 
 addBtn.addEventListener("click",(e) =>{  
     e.preventDefault(); //Hago q no se recargue la pagina
 
     const text = input.value; //esto cuando se pique el + va a aggarar el valor q esta en el input y va a guardarlo
-    const li = document.createElement('li');
+
+    if(text !== ''){
+        const li = document.createElement('li');
     const p = document.createElement('p');
     p.textContent = text;
 
@@ -17,6 +52,10 @@ addBtn.addEventListener("click",(e) =>{
 
     input.value = ""; // cada vez q quiero agregar una tarea se pone en blanco el input para q no haya q borrarlo
     empty.style.display = "none";
+
+    }
+
+    
 });
 
 function addDeleteBtn (){
@@ -28,6 +67,18 @@ function addDeleteBtn (){
     deleteBtn.addEventListener('click', (e) =>{
         const item = e.target.parentElement; // se refiere al boton 
         ul.removeChild(item);
+
+        const items = document.querySelectorAll('li');
+        
+        if(items.length === 0){
+        empty.style.display = "block";
+        }
+
+
+
     });
     return deleteBtn;
 }
+
+
+
