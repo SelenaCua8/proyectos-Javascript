9bf5f6ec57374d58effd11b2a71b16b6705698c5
Selena Cuadra <selenac99@hotmail.com>
2023-03-25 21:36:53 -0300

    agregando la funcion para arrancar el juego

---
+++ proyecto5/index.js
@@ -0,0 +1,40 @@
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalid;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector("#turn");
+const topLeft = document.querySelector("#topleft");
+const topRight = document.querySelector("#topright");
+const bottomLeft = document.querySelector("#bottomletf");
+const bottomRight = document.querySelector("#bottomrigth");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start"); 
+
+
+strictButton.addEventListener('click', (e) => {
+    if (strictButton.checked == true) {
+        strict = true;
+    } else {
+        strict = false;
+    }
+})
+onButton.addEventListener('click', (e) =>{
+    if (onButton.checked == true){
+        on = true;
+        turnCounter.innerHTML = "-";
+    } else {
+        on = false;
+        turnCounter.innerHTML = "";
+    }
+})
+
+
