4f3e7f22a0dae03e4469fc695c9e8d0f6c49575d
Selena Cuadra <selenac99@hotmail.com>
2023-03-29 11:12:14 -0300

    getters y setters

---
+++ proyectosJavaScript/proyecto 6/index.js
@@ -0,0 +1,14 @@
+class Book {   //parecido a una function pero no retorna nada, solo tener variables(propiedades) y funciones(metodos)
+    title = '1984';
+    author = 'George Orwell';
+    price = 350;
+
+
+
+}
+
+
+//creación de primer objeto
+const book1 = new Book();
+
+console.log(book1.title);
