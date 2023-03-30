12e9a2e69717c8a298fc0524a69ad4cf651621b7
Selena Cuadra <selenac99@hotmail.com>
2023-03-30 17:32:23 -0300

    creando clases objetos, metodos y propiedadaes

---
+++ proyectosJavaScript/proyecto7/models/question.js
@@ -0,0 +1,14 @@
+class Question {
+
+    constructor () {
+        this.text  = "alguna pregunta";
+        this.choices = ["asdasd", "asdasd"]; 
+        this.answer = "asdasd"; 
+    }
+
+
+}
+
+
+console.log("questionnnn");
+
