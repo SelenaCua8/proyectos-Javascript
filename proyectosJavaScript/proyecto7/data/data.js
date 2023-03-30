a371629ed4cbfadea9c21296d2d562e386c8f56e
Selena Cuadra <selenac99@hotmail.com>
2023-03-30 17:21:33 -0300

    importando

---
+++ proyectosJavaScript/proyecto7/data/data.js
@@ -0,0 +1,35 @@
+export const data = [
+    {
+        question: "Cual de las siguientes opciones no es un editor de codigo?",
+        choice: [
+            "vim",
+            "vscode",
+            "emacs",
+            "word",
+        ],
+        answer: "word"
+    },
+
+    {
+        question: "Que lenguaje no es orientado a objetos?",
+        choice: [
+            "java",
+            "haskell",
+            "c++",
+            "Python",
+        ],
+        answer: "haskell"
+    },
+    {
+        question: " que lenguaje no sirve para estilizar sitios web?",
+        choice: [
+            "stylus",
+            "less",
+            "sass",
+            "PostCSS",
+            "NextCss",
+        ],
+        answer: "PostCSS"
+    },
+
+];
